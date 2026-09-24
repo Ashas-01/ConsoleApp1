d1a0bebbc5ec224d9ff5d847bb3cc16ddc459632
Ashas-01 <qwerkod123@gmail.com>
2026-09-24 17:12:41 +0700

    Introduce BadMethod with poor practices
    Added a new method 'BadMethod' with complex logic and code smells.

--- Calculator.cs
+++ Calculator.cs
@@ -42,15 +42,84 @@ namespace CalculatorApp
             }
             return result;
         }
-                public int Power(int number, int exponent)
+        public int BadMethod(int a, int b, int c, int d, int e)
+{
+    // Куча code smells + плохие практики
+    int x = 0;
+    int y = 0;
+    int z = 0;
+    int result = 0;
+
+    if (a > 0)
+    {
+        if (b > 0)
         {
-            // Намеренно не самый лучший код
-            int result = 1;
-            for (int i = 0; i < exponent; i++)
+            if (c > 0)
             {
-                result = result * number;
+                if (d > 0)
+                {
+                    if (e > 0)
+                    {
+                        x = a + b;
+                        y = c + d;
+                        z = e * 2;
+                        result = x + y + z;
+                    }
+                    else
+                    {
+                        result = a * b * c * d * e;
+                    }
+                }
+                else
+                {
+                    result = a - b - c - d - e;
+                }
             }
-            return result;
+            else
+            {
+                result = a * a * a;
+            }
         }
+        else
+        {
+            result = b * b;
+        }
+    }
+    else
+    {
+        result = -1;
+    }
+
+    // Дублирование кода
+    if (result > 100)
+    {
+        Console.WriteLine("Большое число");
+        Console.WriteLine("Большое число");
+        Console.WriteLine("Большое число");
+    }
+
+    // Плохие имена + магические числа
+    int temp1 = result * 42;
+    int temp2 = temp1 / 7;
+    int temp3 = temp2 + 13;
+
+    // Пустой catch (очень плохо)
+    try
+    {
+        int crash = 10 / a;
+    }
+    catch
+    {
+    }
+
+    // Ещё одно дублирование
+    if (temp3 > 50)
+    {
+        Console.WriteLine("Большое число");
+        Console.WriteLine("Большое число");
+    }
+
+    return temp3;
+}
     }
 }
